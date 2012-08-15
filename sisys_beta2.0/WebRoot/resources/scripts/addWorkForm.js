3c8821f07e3c8c6a130f680e7fbab1f69c1ce474
cheng04119 <cheng04119@gmail.com>
2012-08-15 03:52:54 +0000

    返工工单js

--- sisys_beta2.0/WebRoot/resources/scripts/addWorkForm.js
+++ sisys_beta2.0/WebRoot/resources/scripts/addWorkForm.js
@@ -100,6 +100,34 @@
     	  }
        }
        
+     //根据工号得到员工姓名
+       function displayStaNo(){
+         var staNo =document.getElementById("staNo").value.trim(); 
+         if(staNo == ""){
+       	  return;
+         }
+         creatXmlHttp();
+         xmlHttp.open("GET", "preAddWorkFormStaNo.action?staNo="+staNo , true);
+         xmlHttp.onreadystatechange = doworkStaNo;
+         xmlHttp.send(null);
+       }
+   
+      function doworkStaNo(){
+         if (xmlHttp.readyState == 4) { // 测试状态是否请求完成
+     		if (xmlHttp.status == 200) { // 如果服务端回应OK
+      			var text = xmlHttp.responseText;
+      			if(text == "error"){
+      				document.getElementById("staName").value = "";
+      				alert("该工号不存在！请重新输入！");
+      			}else{
+      				document.getElementById("staName").value = text;//将内容放入
+      			}       				
+     		}else { //页面不正常  
+               window.alert("您所请求的页面有异常。");  
+           }  
+   	  }
+      }
+       
      //根据工号得到责任人姓名
        function displayRespNo(){
          var staNo =document.getElementById("respNo").value.trim(); 
