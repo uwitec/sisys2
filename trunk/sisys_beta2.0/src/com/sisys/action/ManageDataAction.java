de5608027d19f8a289ea4262e8c44a3ee4c5f17c
gs464gan@gmail.com <gs464gan@gmail.com@46e821c6-33b4-268b-e73b-13f785ecba1c>
2012-09-25 15:12:06 +0000

    批量导入

--- trunk/sisys_beta2.0/src/com/sisys/action/ManageDataAction.java
+++ trunk/sisys_beta2.0/src/com/sisys/action/ManageDataAction.java
@@ -1,9 +1,13 @@
 package com.sisys.action;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.OutputStream;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
+
 
 import com.sisys.action.BaseAction;
 import com.sisys.dao.StaffDAO;
@@ -89,14 +93,68 @@ public class ManageDataAction extends BaseAction{
 	
 	//产品成本表导入
 	public String proImport() throws Exception{
+
 		if(!myFile.exists()){
 			return ERROR;
 		}
-		String result = manageDataService.proImport(myFile);
-		
+		String result=manageDataService.proImport(myFile);
 		return result;
 	}
+	//产品成本表批量导入
+	List<String> errorPro=new ArrayList<String>();
+	public  String ReadFile(String filepath) throws Exception { 				
+		String result_1=new String();
+		result_1="success";
+		try { 
+			File file = new File(filepath);
+		//File file = new File("C:/产品表"); 
+		if (!file.isDirectory()) { 
+			ManageDataService manageDataService = new ManageDataService();
+			String result = manageDataService.proImport(file);
+			if(!result.equals("success")){
+				errorPro.add(file.getName());
+				result_1="errormore";
+				
+			}
 
+		} 
+		else if (file.isDirectory()) {  
+		File[] filelist = file.listFiles(); 
+		for (int i = 0; i < filelist.length; i++) {  
+		if (!filelist[i].isDirectory()) { 
+			System.out.println(filelist[i]);
+			ManageDataService manageDataService = new ManageDataService();
+			String result = manageDataService.proImport(filelist[i]);
+			System.out.println(result);
+			if(!result.equals("success")){
+				errorPro.add(filelist[i].getName());
+				result_1="errormore";				
+			}
+
+		} 
+		else if (filelist[i].isDirectory()) { 
+			ReadFile(filelist[i].toString()); 
+		} 
+		} 
+
+		} 
+
+		} 
+		catch (FileNotFoundException e) { 
+		System.out.println("readfile() Exception:" + e.getMessage()); 
+		} 
+		
+		return result_1; 
+		}
+	
+	public String moreproImport() throws Exception {
+		String filepath = request.getParameter("path");
+		String result=this.ReadFile(filepath);
+		request.setAttribute("errorPro", errorPro);
+		System.out.println(errorPro+"	"+errorPro.size());
+		return result;
+	}
+	
 	public String getTitle() {
 		return title;
 	}
